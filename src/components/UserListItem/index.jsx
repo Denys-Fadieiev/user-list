10c300fd7686ec2aaa762d287a869303be2c957b
Denys-Fadieiev <dencheek0911@gmail.com>
2023-01-03 17:55:56 +0200

    correct UserListItem

--- src/components/UserListItem/index.jsx
+++ src/components/UserListItem/index.jsx
@@ -1,44 +1,46 @@
-import React, { Component } from 'react';
-import { FaTrash } from 'react-icons/fa'
-import styles from './UserListItem.module.scss';
+import classNames from "classnames";
+import React, { Component } from "react";
+import { FaTrash } from "react-icons/fa";
+import styles from "./UserListItem.module.scss";
 
-const { wrapperListItemSelect, wrapperListItemDefault } = styles;
+const { wrapperListItemSelect } = styles;
 
 class UserListItem extends Component {
-  render() {
-    const { usersData, handleSelect } = this.props;
-    // const { id, name, nickName, img, age, gender, isSelected, } = usersData;
+	render() {
+		const { usersData, handleSelect, handleDelete } = this.props;
 
-    const userListItem = usersData.map(({ id, name, nickName, img, age, gender, isSelected, }) => (
-      <li
-        className={`${styles.wrapperListItem} ${
-        isSelected ? wrapperListItemSelect : wrapperListItemDefault
-      }`}
-        id={id}
-        key={id}
-        onClick={() => handleSelect(id)}
-      >
-        <img className={styles.userImg} src={img.src} alt={img.alt} />
-				<div className={styles.wrapperItem}>
-					<div>
-						<p className={styles.name}>{name}</p>
-						<p className={styles.nickName}>{nickName}</p>
+		const userListItem = usersData.map(
+			({ id, name, nickName, img, age, gender, isSelected }) => (
+				<li
+					className={classNames(styles.wrapperListItem, {[wrapperListItemSelect] : isSelected})}
+					id={id}
+					key={id}
+				>
+					<div 
+            className={styles.wrapperItem}
+            onClick={() => handleSelect(id)}>
+						<img className={styles.userImg} src={img.src} alt={img.alt} />
+							<div>
+								<p className={styles.name}>{name}</p>
+								<p className={styles.nickName}>{nickName}</p>
+							</div>
 					</div>
 					<div className={styles.wrapperBtn}>
-						<button className={styles.btn} type="submit">
-							<FaTrash/>
+						<button
+							className={styles.btn}
+							type="submit"
+							onClick={() => handleDelete(id)}
+              
+						>
+							<FaTrash />
 						</button>
 					</div>
-				</div>
-      </li>
-    ));
+				</li>
+			)
+		);
 
-    return (
-      <>
-        {userListItem}
-      </>
-    );
-  }
+		return <>{userListItem}</>;
+	}
 }
 
 export default UserListItem;
